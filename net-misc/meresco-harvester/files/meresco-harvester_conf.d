16889f3f4023694b6a61a6c1813a690a0f608e97
Wim Muskee <wimmuskee@gmail.com>
2010-07-22 18:24:19 +0200

    meresco-harvester: Deleted 5.2.2 and 5.2.3.

--- net-misc/meresco-harvester/files/meresco-harvester_conf.d
+++
@@ -1,9 +0,0 @@
-# conf.d file for meresco-harvester
-#
-# HARVESTOPTS controls the meresco specific harvest options
-# consult the Sahara manual for details
- 
-HARVESTOPTS="--domain="
-
-HARVEST_USER="apache"
-HARVEST_GROUP="apache"
